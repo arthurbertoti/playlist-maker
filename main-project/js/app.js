5dadb7895dc72339d82eb4fbd1883704ff98ddb9
Arthur G. Bertoti <artgbertoti@gmail.com>
2022-08-14 22:59:22 -0300

    apagado os outros 2 repositórios

--- main-project/js/app.js
+++
@@ -1,7 +0,0 @@
-import { TrackController } from './controllers/track-controller.js';
-const controller = new TrackController();
-const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.addMusic();
-});
