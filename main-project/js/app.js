848edb569ae720d01e9ca7047e12bb08757dfbf6
Arthur G. Bertoti <artgbertoti@gmail.com>
2022-08-14 22:58:36 -0300

    main-project ta quebrado
    uso agora o project, apaguei o main-project
    
    para usar o servidor, deve-se digitar no terminal (nesta locallização= D:\playlist-maker)
    > npm run start

---
+++ main-project/js/app.js
@@ -0,0 +1,7 @@
+import { TrackController } from './controllers/track-controller.js';
+const controller = new TrackController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.addMusic();
+});
